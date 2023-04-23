0f76bf5745f73885af8cf03cd362f40a74e19837
Noso67 <128983505+Noso67@users.noreply.github.com>
2023-04-23 13:17:20 +0530

    Update botconfig.js

--- botconfig.js
+++ botconfig.js
@@ -4,9 +4,9 @@ module.exports = {
   DefaultPrefix: process.env.Prefix || ">", // Default prefix, Server Admins can change the prefix
   Port: 3000, //Which port website gonna be hosted
   SupportServer: "https://discord.gg/sbySMS7m3v", // Support Server Link
-  Token: process.env.Token || "", // Discord Bot Token
-  ClientID: process.env.Discord_ClientID || "", // Discord Client ID
-  ClientSecret: process.env.Discord_ClientSecret || "", // Discord Client Secret
+  Token: process.env.Token || "MTA5OTYwMTMyNDE1ODQ4ODYwOQ.Gx44AK.QwddPMgggyba043AxjWXdZxXInbmX3ynoE2eDk", // Discord Bot Token
+  ClientID: process.env.Discord_ClientID || "1099601324158488609", // Discord Client ID
+  ClientSecret: process.env.Discord_ClientSecret || "zSpG95Pc9vhBp4ffq6PF8-qo7l29MLp8", // Discord Client Secret
   Scopes: ["identify", "guilds", "applications.commands"], // Discord OAuth2 Scopes
   ServerDeafen: true, // If you want bot to stay deafened
   DefaultVolume: 100, // Sets the default volume of the bot, You can change this number anywhere from 1 to 9007199254740991 (JS Integer limit. If you do set it to that, you're a monster.)
@@ -30,16 +30,16 @@ module.exports = {
   // Lavalink server; public lavalink -> https://lavalink-list.darrennathanael.com/; create one yourself -> https://darrennathanael.com/post/how-to-lavalink
   Lavalink: {
     id: "Main", //- Used for indentifier. You can set this to whatever you want.
-    host: "", //- The host name or IP of the lavalink server.
-    port: 80, // The port that lavalink is listening to. This must be a number!
-    pass: "", //- The password of the lavalink server.
-    secure: false, // Set this to true if the lavalink uses SSL. if not set it to false.
+    host: "lavalink-replit.dragon3421.repl.co", //- The host name or IP of the lavalink server.
+    port: 443, // The port that lavalink is listening to. This must be a number!
+    pass: "oddcoder", //- The password of the lavalink server.
+    secure: true, // Set this to true if the lavalink uses SSL. if not set it to false.
     retryAmount: 200, //- The amount of times to retry connecting to the node if connection got dropped.
     retryDelay: 40, //- Delay between reconnect attempts if connection is lost.
   },
   // Spotify Integration, allows you to enter a spotify link.
   Spotify: {
-    ClientID: process.env.Spotify_ClientID || "", // Spotify Client ID
-    ClientSecret: process.env.Spotify_ClientSecret || "", // Spotify Client Secret
+    ClientID: process.env.Spotify_ClientID || "298a0c95fb534ee2be6abd7f2af5f292", // Spotify Client ID
+    ClientSecret: process.env.Spotify_ClientSecret || "55e59add3ab5405fb87ca2f90c901a64", // Spotify Client Secret
   },
 };
